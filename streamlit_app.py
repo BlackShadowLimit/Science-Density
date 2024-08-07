5d584a87f06a1357f95b26cfd5e6bb784103941c
BlackShadowLimit <99549300+BlackShadowLimit@users.noreply.github.com>
2024-08-07 16:27:40 +0800

    Delete brain.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,63 @@
 import streamlit as st
-import SD_Brain
 import speech_recognition as sr
 from gtts import gTTS
 from pydub import AudioSegment
 import simpleaudio as sa
-import os
+from groq import Groq
+import google.generativeai as genai
+from pydub.playback import play
 
 # Initialize the recognizer
 r = sr.Recognizer()
 play_obj = None
+
+
+# Initialize clients with your API keys
+groq_client = Groq(api_key="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")
+genai.configure(api_key="AIzaSyBwMwo1r2qQ3walNFTn6dGfJaVHv_ukAzE")
+
+model_name = 'llama3-8b-8192'
+
+# System message for the AI model
+sys_msg = (
+    'You are Science Density, an AI English speaking coach. Your task is to help the user practice and improve their spoken English. '
+    'Provide constructive feedback on their grammar, vocabulary, and pronunciation. Encourage the user and give them '
+    'tips on how to improve their spoken English skills. Make sure your responses are clear and easy to understand.'
+)
+
+# Generation configuration
+generation_config = {
+    'temperature': 0.7,
+    'top_p': 1,
+    'top_k': 1,
+    'max_output_tokens': 2048
+}
+
+# Safety settings
+safety_settings = [
+    {
+        'category': 'HARM_CATEGORY_HARASSMENT',
+        'threshold': 'BLOCK_NONE'
+    }
+]
+
+# Initialize the Generative Model
+model = genai.GenerativeModel(
+    'gemini-1.5-flash-latest',
+    generation_config=generation_config,
+    safety_settings=safety_settings
+)
+
+
+# Function to generate a prompt response
+def groq_prompt(prompt):
+    convo = [
+        {'role': 'system', 'content': sys_msg},
+        {'role': 'user', 'content': prompt}
+    ]
+    chat_completion = groq_client.chat.completions.create(messages=convo, model=model_name)
+    response = chat_completion.choices[0].message
+    return response.content  # Correctly accessing the content attribute
 
 # Use the microphone as the source for input
 def transcriber():
@@ -54,7 +103,7 @@ if st.button("Start Speak"):
     text = transcriber()
     if text:
         st.write(f"USER: {text}")
-        response = SD_Brain.groq_prompt(text)
+        response = groq_prompt(text)
         speak(response)
 
         # Play new audio
